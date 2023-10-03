ae190fbdb1cb1b3741a24f9b39a647cc3fc17755
ZakharTsarapov <tsarapovs@gmail.com>
2023-10-03 14:54:37 +0300

    hw-03 done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,55 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person) {
+    if (this.door === true) {
+      this.tenants.push(person);
+    } else {
+      console.log("this door is closed");
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+
+class MyHouse extends House {
+    public openDoor(key: Key): boolean {
+        if(key.getSignature() === this.key.getSignature()) {
+            this.door = true
+            return this.door
+        } else {
+           return this.door = false
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +58,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
